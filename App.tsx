5c1fc867cc6975884b260af8c42d9cdd9afa6079
Ankit Thakur <ankitthakur@192.168.1.3>
2023-07-12 06:26:29 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppPro from './src/components/AppPro';
+const App = () => {
+  return <AppPro />;
+};
+
+export default App;
---
+++ src/components/AppPro.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+const AppPro = (): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+          This is my first app
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // alignItems: 'center',
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+export default AppPro;
